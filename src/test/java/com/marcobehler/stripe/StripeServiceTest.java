70f4e5cd7f6be2be377a82a96f7d9e25e12d389f
Marco Behler <marco@marcobehler.com>
2018-02-15 10:17:20 +0100

    further work

--- src/test/java/com/marcobehler/stripe/StripeServiceTest.java
+++ src/test/java/com/marcobehler/stripe/StripeServiceTest.java
@@ -19,12 +19,10 @@ public class StripeServiceTest {
     @Autowired
     private StripeService stripeService;
 
-
     @Test
     public void charge_is_successful() {
         Charge charge = stripeService.charge("tok_visa", 1123);
         assertThat(charge).isNotNull();
         assertThat(charge.getStatus()).isEqualTo("succeeded");
     }
-
 }
