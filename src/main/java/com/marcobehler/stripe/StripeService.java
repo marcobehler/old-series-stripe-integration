acba09e768e43f6de7e1e15e172961f433031799
Marco Behler <marco@marcobehler.com>
2018-02-16 10:04:02 +0100

    further work

--- src/main/java/com/marcobehler/stripe/StripeService.java
+++ src/main/java/com/marcobehler/stripe/StripeService.java
@@ -1,10 +1,14 @@
 package com.marcobehler.stripe;
 
 import com.stripe.Stripe;
+import com.stripe.exception.CardException;
 import com.stripe.model.Charge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -21,8 +25,12 @@ public class StripeService {
 
     private String stripeSecretKey;
 
-    public StripeService(@Value("${stripe.secretKey}") String stripeSecretKey) {
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public StripeService(@Value("${stripe.secretKey}") String stripeSecretKey, JavaMailSender mailSender) {
         this.stripeSecretKey = stripeSecretKey;
+        this.mailSender = mailSender;
     }
 
     public Charge charge(String token, Integer amountInCents) {
@@ -36,9 +44,29 @@ public class StripeService {
 
         try {
             return Charge.create(params);
-        } catch (Exception e) {
+        }
+        catch (CardException e) {
+            handleCardException(e);
+            return null;
+        }
+        catch (Exception e) {
             logger.error("Problem charging card", e);
             return null;
+        }
+    }
+
+    private void handleCardException(CardException e) {
+        String code = e.getCode();
+
+        if (code.equals("expired_card")) {
+            SimpleMailMessage m = new SimpleMailMessage();
+            m.setFrom("your@mail.com");
+            m.setTo("customers@mail.com");
+            m.setSubject("dude, where's my money??");
+            m.setText("please renew your credit card details !!!");
+            mailSender.send(m);
+        } else {
+            // TODO
         }
     }
 
--- src/test/java/com/marcobehler/stripe/StripeServiceTest.java
+++ src/test/java/com/marcobehler/stripe/StripeServiceTest.java
@@ -1,11 +1,13 @@
 package com.marcobehler.stripe;
 
 import com.stripe.model.Charge;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.subethamail.wiser.Wiser;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,10 +21,29 @@ public class StripeServiceTest {
     @Autowired
     private StripeService stripeService;
 
+    private Wiser wiser;
+
+    @Before
+    public void setUp() throws Exception {
+        wiser = new Wiser(25);
+        wiser.start();
+    }
+
     @Test
     public void charge_is_successful() {
         Charge charge = stripeService.charge("tok_visa", 1123);
         assertThat(charge).isNotNull();
         assertThat(charge.getStatus()).isEqualTo("succeeded");
+    }
+
+    @Test
+    public void credit_card_is_expired() {
+        Charge charge = stripeService.charge("tok_chargeDeclinedExpiredCard", 1123);
+        assertThat(charge).isNull();
+
+        assertThat(wiser.getMessages()).hasSize(1);
+        assertThat(wiser.getMessages()).extracting("mimeMessage.subject")
+                .contains("dude, where's my money??");
+
     }
 }
