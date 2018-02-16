9793752bb64067c48639daf294c09af956897f22
Marco Behler <marco@marcobehler.com>
2018-02-16 11:10:16 +0100

    networking error

--- src/main/java/com/marcobehler/stripe/StripeApiKeyChecker.java
+++ src/main/java/com/marcobehler/stripe/StripeApiKeyChecker.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
  */
 @Component
+@Profile("production")
 public class StripeApiKeyChecker implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(StripeApiKeyChecker.class);
--- src/main/java/com/marcobehler/stripe/StripeService.java
+++ src/main/java/com/marcobehler/stripe/StripeService.java
@@ -1,16 +1,22 @@
 package com.marcobehler.stripe;
 
 import com.stripe.Stripe;
+import com.stripe.exception.APIConnectionException;
 import com.stripe.exception.CardException;
 import com.stripe.model.Charge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,14 +29,38 @@ public class StripeService {
 
     private static final Logger logger = LoggerFactory.getLogger(StripeService.class);
 
+    private final Environment env;
+
     private String stripeSecretKey;
 
     private JavaMailSender mailSender;
 
     @Autowired
-    public StripeService(@Value("${stripe.secretKey}") String stripeSecretKey, JavaMailSender mailSender) {
+    public StripeService(@Value("${stripe.secretKey}") String stripeSecretKey, JavaMailSender mailSender, Environment env) {
         this.stripeSecretKey = stripeSecretKey;
         this.mailSender = mailSender;
+        this.env = env;
+    }
+
+    @PostConstruct
+    public void setup() {
+        String proxyHost = env.getProperty("http.proxyHost");
+        Integer proxyPort = env.getProperty("http.proxyPort", Integer.class);
+
+        if (StringUtils.hasText(proxyHost) && proxyPort != null) {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            Stripe.setConnectionProxy(proxy);
+        }
+
+        Integer stripeConnectTimeout = env.getProperty("stripe.connectTimeout", Integer.class);
+        if (stripeConnectTimeout != null) {
+            Stripe.setConnectTimeout(stripeConnectTimeout); // in milliseconds, so 3 seconds
+        }
+
+        Integer stripeReadTimeout = env.getProperty("stripe.readTimeout", Integer.class);
+        if (stripeReadTimeout != null) {
+            Stripe.setReadTimeout(stripeReadTimeout);
+        }
     }
 
     public Charge charge(String token, Integer amountInCents) {
@@ -48,6 +78,9 @@ public class StripeService {
         catch (CardException e) {
             handleCardException(e);
             return null;
+        } catch (APIConnectionException e) {
+            handleAPIConnectionException(e);
+            return null;
         }
         catch (Exception e) {
             logger.error("Problem charging card", e);
@@ -55,9 +88,13 @@ public class StripeService {
         }
     }
 
+    private void handleAPIConnectionException(APIConnectionException e) {
+        // TODO what to do? retry? what if transaction happened on stripes side?  contact customer? contact yourself?
+        throw new RuntimeException(e);
+    }
+
     private void handleCardException(CardException e) {
         String code = e.getCode();
-
         if (code.equals("expired_card")) {
             SimpleMailMessage m = new SimpleMailMessage();
             m.setFrom("your@mail.com");
